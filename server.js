781b3a0f835a2bd70ebdab6bc4314e8d55791239
Chris Bistline <chris.bistline@gmail.com>
2019-06-10 21:46:20 -0500

    ran eslint to fix formatting

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+    app.use(express.static("client/build"));
 }
 
 /* UPDATE THIS ONCE ROUTING HAS BEEN CREATED
@@ -24,10 +24,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglis
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
