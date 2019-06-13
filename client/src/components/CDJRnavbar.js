240ab15bc8de19cd7cc6ab44311a2a2e6bd224fc
Chris Bistline <chris.bistline@gmail.com>
2019-06-13 18:17:08 -0500

    reconfigured as react-bootstrap components

--- client/src/components/CDJRnavbar.js
+++ client/src/components/CDJRnavbar.js
@@ -1,64 +1,50 @@
 import React from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 const CDJRnavbar = () => {
     return (
-        <Navbar>
-            <Nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top" style={{ "boxShadow": "0px 4px 2px -1px rgba(38,37,38,1)" }}>
-                <a className="navbar-brand" href="#">
+        <div>
+            <Navbar collapseOnSelect expand="xl" bg="light" variant="light" fixed="top" style={{ "boxShadow": "0px 4px 2px -1px rgba(38,37,38,1)" }}>
+                <Navbar.Brand href="#home">
                     <img src="./assets/img/logo.png" width="300" height="50" alt="dealerLogo" />
-                </a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#"> <span className="sr-only">(current)</span><i className="fas fa-home"></i> HOME</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">NEW</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">USED</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">SPECIALS</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">SELL & TRADE</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">FINANCE & LEASE</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">PARTS</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">SERVICE</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">COLLISION</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">ABOUT</a>
-                        </li>
-                    </ul>
-                    <span className="navbar-text ml-auto">
-                        <p>Parts
-                <br />615-538-9033</p>
-                    </span>
-                    <div className="btn-toolbar ml-auto" role="toolbar" aria-label="Toolbar with button groups">
-                        <div className="btn-group ml-2" role="group" aria-label="First group">
-                            <button type="button" className="btn btn-outline-secondary"><i className="fa fa-calendar"> SERVICE</i></button>
-                            <button type="button" className="btn btn-outline-secondary"><i className="fa fa-map-marker"> MAP</i></button>
-                            <button type="button" className="btn btn-outline-secondary"><i className="fa fa-envelope"> CONTACT</i></button>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home"><span className="sr-only">(current)</span><i className="fas fa-home"></i> HOME</Nav.Link>
+                        <Nav.Link href="#new">NEW</Nav.Link>
+                        <Nav.Link href="#used">USED</Nav.Link>
+                        <Nav.Link href="#specials">SPECIALS</Nav.Link>
+                        <Nav.Link href="#selltrade">SELL & TRADE</Nav.Link>
+                        <Nav.Link href="#financelease">FINANCE & LEASE</Nav.Link>
+                        <Nav.Link href="#parts">PARTS</Nav.Link>
+                        <Nav.Link href="#service">SERVICE</Nav.Link>
+                        <Nav.Link href="#collision">COLLISION</Nav.Link>
+                        <Nav.Link href="#about">ABOUT</Nav.Link>
+
+
+                    </Nav>
+                    <Nav>
+                        <span className="navbar-text ml-auto">
+                            <p>Parts
+                            <br />615-538-9033</p>
+                        </span>
+                        <div className="btn-toolbar ml-auto" role="toolbar" aria-label="Toolbar with button groups">
+                            <div className="btn-group ml-2" role="group" aria-label="First group">
+                                <button type="button" className="btn btn-outline-secondary"><i className="fa fa-calendar"> SERVICE</i></button>
+                                <button type="button" className="btn btn-outline-secondary"><i className="fa fa-map-marker"> MAP</i></button>
+                                <button type="button" className="btn btn-outline-secondary"><i className="fa fa-envelope"> CONTACT</i></button>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </Nav>
-        </Navbar>
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
+
+
+        </div>
     );
 };
 
