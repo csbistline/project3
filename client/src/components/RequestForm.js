447d475ee5084e1d6f722ed08015043e092da21b
GutleberB94 <gutleberb@gmail.com>
2019-06-17 19:49:42 -0500

    setup request form router and boilerplate

---
+++ client/src/components/RequestForm.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import PartsSpecCard from "./PartsSpecCard";
+import PartsSpec from "../PartsSpec.json";
+import Contacts from "./Contacts";
+
+
+class RequestForm extends Component {
+
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="row pt-4" >
+                    <div className="col-md-8 pt-4 d-flex flex-wrap justify-content-around">
+
+
+                        </div>
+
+                    <div className="col-sm-4 pt-4">
+                        <Contacts />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default RequestForm;
