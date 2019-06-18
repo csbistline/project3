b1ee35075d87eb7a6d15e98a9df046e2899bb02d
GutleberB94 <gutleberb@gmail.com>
2019-06-17 20:22:07 -0500

    added placeholder form fields

--- client/src/components/RequestForm.js
+++ client/src/components/RequestForm.js
@@ -8,7 +8,24 @@ class RequestForm extends Component {
 
     state = {
 
+        firstName: "",
+        lastName: "",
+        phoneNum: "",
+        email: "",
+        VIN: "",
+        message: ""
+
     }
+
+    handleInputChange = event => {
+        // Getting the value and name of the input which triggered the change
+        const { name, value } = event.target;
+
+        // Updating the input's state
+        this.setState({
+            [name]: value
+        });
+    };
 
     render() {
         return (
@@ -16,10 +33,27 @@ class RequestForm extends Component {
                 <div className="row pt-4" >
                     <div className="col-md-8 pt-4 d-flex flex-wrap justify-content-around">
 
+                        <div className="jumbotron">
+                            <form>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Email address</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">Password</label>
+                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleFormControlTextarea1">Example textarea</label>
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
 
-                            <h1>This will be the request form</h1>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
+
                         </div>
-
+                    </div>
                     <div className="col-sm-4 pt-4">
                         <Contacts />
                     </div>
