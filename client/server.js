50572c7d65fb2a8a05e00426af5da8e3f64d7ba5
Chris Bistline <chris.bistline@gmail.com>
2019-06-19 18:38:25 -0500

    removed unnecessary server file from client directory

--- client/server.js
+++ client/server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+/* const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -22,3 +22,4 @@ if (process.env.NODE_ENV === "production") {
 app.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
+*/
