bc29c0052959772009b91cedd46ef8f87622b5d3
Andy Pack <thepackpater@gmail.com>
2019-06-22 18:13:50 -0500

    Connected CustDataTable to PartsData

--- client/src/components/CusDataTable.js
+++ client/src/components/CusDataTable.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import API from "../utils/partsAPI";
 import Button from "react-bootstrap/Button";
 import Table from 'react-bootstrap/Table'
 
 
 class CusDataTable extends Component {
+  state = {
+    CusPartsQuery: []
+}
+componentDidMount() {
+    this.loadPartsQuery();
+}
+loadPartsQuery = () => {
+    API.getPartsRequests()
+        .then(res => {
+            console.log(res);
+            console.log(res.data)
+            this.setState({ CusPartsQuery: res.data })
+        })
+        .catch(err => console.log(err));
+};
   render() {
     return(
-      <div>
+      <div className="containerFluid">
         <br></br>
         <Table striped bordered hover>
           <thead>
@@ -20,24 +36,30 @@ class CusDataTable extends Component {
             </tr>
           </thead>
           <tbody>
-            {/* insert data from CustQuery DB */}
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
+            {this.state.CusPartsQuery.map(Query =>(
+            <tr  key={Query._id}>
+              <td className="align-middle">{Query.firstName}{Query.lastName}</td>
+              <td className="align-middle">{Query.phoneNumber}</td>
+              <td className="align-middle">{Query.email}</td>
+              <td className="align-middle">{Query.vin}</td>
+              <td className="align-middle">
               {/* link to specific QueryCard */}
+              <Button variant="link"
+                 data-id={Query._id}>
+                 View Query/Assign Tech
+              </Button>
+              </td>
+              <td>Status Toggle</td>
+            </tr>
+            ))}
+            {/* <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
               <Button variant="link">View Query/Assign Tech</Button>
               <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <Button variant="link">View Query/Assign Tech</Button>
-              <td></td>
-            </tr>
+            </tr> */}
           </tbody>
         </Table>
         </div>
