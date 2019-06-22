2844ab257f7a14bf96e46c0cba38cb15e2b0f19a
Andy Pack <thepackpater@gmail.com>
2019-06-22 11:49:17 -0500

    updated CusDataTable, QueryCard, TechSelectModal

---
+++ client/src/components/SelectTechModal.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {Button, Modal} from 'react-bootstrap';
+import SelectTech from './SelectTech';
+
+
+
+class SelectTechModal extends React.Component {
+    constructor(props, context) {
+      super(props, context);
+  
+      this.handleShow = this.handleShow.bind(this);
+      this.handleClose = this.handleClose.bind(this);
+  
+      this.state = {
+        show: false,
+      };
+    }
+  
+    handleClose() {
+      this.setState({ show: false });
+    }
+  
+    handleShow() {
+      this.setState({ show: true });
+    }
+  
+    render() {
+      return (
+        <>
+          <Button variant="primary" onClick={this.handleShow}>
+              Assign Tech
+          </Button>
+  
+          <Modal show={this.state.show} onHide={this.handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Parts Specialists</Modal.Title>
+            </Modal.Header>
+                <Modal.Body>
+                    <SelectTech />
+                </Modal.Body>
+            <Modal.Footer>
+              {/* <Button variant="primary" onClick={this.handleClose}>
+                Close
+              </Button> */}
+             
+            </Modal.Footer>
+          </Modal>
+        </>
+      );
+    }
+  }
+  export default SelectTechModal;
