39023d9dd8bfddd0f31b342da66cadc668d9da73
Andy Pack <thepackpater@gmail.com>
2019-06-22 11:02:13 -0500

    added query card and custDataTable component

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Main from "./components/Main";
 import PartsTeam from "./components/PartsTeam";
 import RequestForm from "./components/RequestForm";
 import SelectTech from "./components/SelectTech";
-// import YourComponent from "./components/YourComponent";
+// import INSERTCOMPONENTNAME from "./components/INSERTCOMPONENTNAME";
 
 class App extends Component {
     render() {
@@ -20,7 +20,7 @@ class App extends Component {
                             <Route exact path="/team" component={PartsTeam} />
                             <Route exact path="/request" component={RequestForm} />
                             <Route exact path="/select" component={SelectTech} />
-                            {/* <Route exact path="/test" component={YourComponent} /> */}
+                            {/* <Route exact path="/test" component={INSERTCOMPONENTNAME} /> */}
                         </Switch>
                     </Wrapper>
                 </div>
--- client/src/components/CusDataTable.js
+++ client/src/components/CusDataTable.js
@@ -1,54 +1,54 @@
-// import moment from 'moment'; // Example for onSort prop
-import React from 'react'; // Import React
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
-import 'node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+import React, { Component } from 'react';
+import Button from "react-bootstrap/Button";
+import Table from 'react-bootstrap/Table'
 
-// fuction for Date if we want to insert:
-// const onSortFunction = {
-//     date(Date) {
-//       return moment(Date, 'Do MMMM YYYY').valueOf();
-//     },
-//   };
 
-var CustQueryData = [{ //IMPORT FROM DB
-}];
+class CusDataTable extends Component {
+  render() {
+    return(
+      <div>
+        <br></br>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Customer Name</th>
+              <th>Customer Phone</th>
+              <th>Customer Email</th>
+              <th>Vin</th>
+              <th>Vehicle Year</th>
+              <th>Vehicle Make</th>
+              <th>Vehicle Model</th>
+              <th>Assign Tech</th>
+              <th>Query Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* insert data from CustQuery DB */}
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <Button variant="link">Assign Tech</Button>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <Button variant="link">Assign Tech</Button>
+              <td></td>
+            </tr>
+          </tbody>
+        </Table>
+        </div>
+        )}};
 
-QueryDeleteButton = (onClick) => {
-    return (
-      <DeleteButton
-        btnText='CustomDeleteText'
-        btnContextual='btn-success'
-        className='my-custom-class'
-        onClick={ e => this.handleDeleteButtonClick(onClick) }/>
-    )};
-
-class CusQueryTable extends React.Component {
-    
-    render() {
-
-        const options = {
-            deleteBtn: this.QueryDeleteButton
-          };
-          const selectRow = {
-            mode: 'checkbox'
-          };
-
-      return (
-        <BootstrapTable selectRow={ selectRow } data={ CustQueryData } striped hover condensed options={ options } deleteRow>
-            <TableHeaderColumn width='100' dataField='id' isKey>CustomerID</TableHeaderColumn>
-            <TableHeaderColumn width='100' dataField='name'>Customer Name</TableHeaderColumn>
-            <TableHeaderColumn width='100' dataField='email'>Customer Email</TableHeaderColumn>
-            <TableHeaderColumn width='100' dataField='Vin'>Vehicle Vin</TableHeaderColumn>
-            <TableHeaderColumn width='100' dataField='phone'>Customer Phone</TableHeaderColumn>
-            <TableHeaderColumn width='100' dataField='Vmake'>Vehicle Make</TableHeaderColumn>
-            <TableHeaderColumn width='100' dataField='Vmodel'>Vehicle Model</TableHeaderColumn>
-            <TableHeaderColumn width='100' dataField='Vyear'>Vehicle Year</TableHeaderColumn>
-            <TableHeaderColumn width='100' dataField='PartsSpec'>Specialist Assigned</TableHeaderColumn>
-            <TableHeaderColumn width='100' dataField='Status'>Query Status</TableHeaderColumn>
-        </BootstrapTable>,  
-        <CusQueryTable />
-      );
-    }
-  }
-
-export default CusQueryTable;
+export default CusDataTable;
