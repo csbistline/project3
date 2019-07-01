16490d397c3b6983372092a37f9d5040e5c45de7
GutleberB94 <gutleberb@gmail.com>
2019-07-01 18:29:55 -0500

    added vin check function

--- client/src/components/RequestForm.js
+++ client/src/components/RequestForm.js
@@ -34,6 +34,20 @@ class RequestForm extends Component {
         });
     }
 
+    vinCheck = (vinNum) => {
+
+        unirest.get(`https://vindecoder.p.rapidapi.com/decode_vin?vin=${vinNum}`)
+            .header("X-RapidAPI-Host", "vindecoder.p.rapidapi.com")
+            .header("X-RapidAPI-Key", "8da2207bdbmsh250beb71e2b17aep1c86a9jsn3a30757a482c")
+            .end(function (result) {
+                if (result.success === true) {
+                    return true
+                } else {
+                    return false
+                }
+            });
+    }
+    
     sendFormData = event => {
         event.preventDefault();
         partsAPI.savePartsRequest({
