cdf58c07487757bc044097851097935744098661
RedSW20 <kevbailey1227@gmail.com>
2019-07-06 10:02:42 -0500

    origin pull conflict merges solved

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -29,21 +29,12 @@ class LoginForm extends Component {
     sendFormData = event => {
         event.preventDefault();
         console.log('handleSubmit')
-<<<<<<< HEAD
         // console.log(this.state.username);
         // console.log(this.state.password);      
 		this._login(this.state.username, this.state.password)
 		this.setState({
 			redirectTo: '/'
 		})
-=======
-        console.log(this.state.username);
-        console.log(this.state.password);
-        this._login(this.state.username, this.state.password)
-        this.setState({
-            redirectTo: '/'
-        })
->>>>>>> ea1ed6a5906d6547c16cdf66a4ac6e3c41e2c810
     };
 
     componentDidMount() {
@@ -95,81 +86,6 @@ class LoginForm extends Component {
                 }
             })
     };
-<<<<<<< HEAD
-      /*partsAPI.savePartsRequest({
-          firstName: this.state.firstName,
-          lastName: this.state.lastName,
-          phoneNumber: this.state.phoneNumber,
-          email: this.state.email,
-          vin: this.state.vin,
-          year: this.state.year,
-          make: this.state.make,
-          model: this.state.model,
-          message: this.state.firstName
-      })
-          .catch(err => console.log(err));*/
-
-
-
-  render() {
-      return (
-          <div>
-              <div className="row pt-4" >
-                  <div className="col-md-8 pt-4 d-flex flex-wrap justify-content-around  animated bounceInUp">
-
-                      <div className="jumbotron">
-                          <h1 className="display-4"><b>Team Login Form</b></h1>
-                          <p className="lead">Enter your username/password below to login.</p>
-                          <hr className="my-4"></hr>
-                          <form>
-
-                              <div className="form-group">
-
-                                  <label htmlFor="username">UserName</label>
-
-                                  <input type="text"
-                                      className="form-control"
-                                      name="username"
-                                      value={this.state.username}
-                                      onChange={this.handleInputChange}
-                                      id="username"
-                                      placeholder="Username">
-
-                                  </input>
-                              </div>
-
-                              <div className="form-group">
-                                  <label htmlFor="password current-password">Password</label>
-
-                                  <input type="password"
-
-                                      className="form-control current-password"
-                                      name="password"
-                                      value={this.state.password}
-                                      onChange={this.handleInputChange}
-                                      id="password"
-                                      placeholder="Password">
-
-                                  </input>
-                              </div>
-
-                              <button type="submit"
-                                  className="btn btn-primary myButton"
-                                  // do we want to have the button disabled if all fields are not filled in? disabled={}
-                                  onClick={this.sendFormData}
-                              >Submit</button>
-                          </form>
-
-                      </div>
-                  </div>
-                  <div className="col-md-4 pt-4">
-                      <Contacts />
-                  </div>
-              </div>
-          </div>
-      );
-  }
-=======
     /*partsAPI.savePartsRequest({
         firstName: this.state.firstName,
         lastName: this.state.lastName,
@@ -243,7 +159,6 @@ class LoginForm extends Component {
             </div>
         );
     }
->>>>>>> ea1ed6a5906d6547c16cdf66a4ac6e3c41e2c810
 
 
 
